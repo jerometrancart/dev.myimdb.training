195d9f69ab8270e290331e1bf05b79c801471a35
Dominique <dominique.feret@ingenidoc.fr>
2023-07-31 16:38:44 +0200

    request ok

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -9,35 +9,15 @@ class MovieController extends Controller
 {
     public function index(Request $request, $genre)
     {
-        //retrieve parameter sort from request
-        $sort = $request->input('sort');
+        $html = "List of movies with genre : <strong>{$genre}</strong><br/>";
 
-        $html = "List of movies with genre: <strong>{$genre}</strong><br/>";
+        $html .= "
+        <ul>
+            <li>Halloween</li>
+            <li>Jaw</li>
+        </ul>";
 
-        //sort list of movies
-        switch ($sort)
-        {
-            case('asc'):
-                $html .= "<ul>
-                                <li>Halloween</li>
-                                <li>Jaw</li>
-                           </ul>";
-                break;
-            case ('desc'):
-                $html .= "<ul>
-                                <li>Jaw</li>
-                                <li>Halloween</li>
-                          </ul>";
-                break;
-            default:
-                $html .= "<ul>
-                                <li>Halloween</li>
-                                <li>Jaw</li>
-                           </ul>";
-                break;
-
-
-        }
+        $html .='Uri :'.$request->path();
         $response =new \Illuminate\Http\Response($html);
         return $response;
     }//
